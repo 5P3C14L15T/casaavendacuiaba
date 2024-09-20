c43ee625148246405d615ea7188cdcfca2bbb3b5
João Victor <joaovictorvieira@outlook.com.br>
2024-09-20 16:50:48 -0400

    form

--- js/script.js
+++ js/script.js
@@ -1,7 +1,16 @@
 // JavaScript para controlar o formulário de pesquisa
-document.querySelector('.search-icon').addEventListener('click', function (e) {
-    e.preventDefault();
-    const searchForm = document.getElementById('search-form');
-    searchForm.classList.toggle('active'); // Alterna a classe 'active' para mostrar ou esconder o formulário
-  });
-  
+document.querySelector(".search-icon").addEventListener("click", function (e) {
+  e.preventDefault();
+  const searchForm = document.getElementById("search-form");
+  searchForm.classList.toggle("active"); // Alterna a classe 'active' para mostrar ou esconder o formulário
+});
+
+const valorMinimoInput = document.getElementById("valor-minimo");
+const valorMaximoInput = document.getElementById("valor-maximo");
+
+valorMinimoInput.addEventListener("change", function () {
+  const valorMinimo = parseInt(this.value);
+  valorMaximoInput.min = valorMinimo + 50000; // O valor mínimo de "Valor Máximo" começa onde o "Valor Mínimo" termina
+  valorMaximoInput.value = valorMinimo + 50000; // Define um valor padrão para "Valor Máximo"
+});
+
