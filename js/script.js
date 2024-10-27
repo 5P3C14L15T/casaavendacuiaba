f299248e4f17ca2e7cf1c475912509f30ede2cfa
João Victor <joaovictorvieira@outlook.com.br>
2024-10-26 23:53:35 -0400

    atualizações

--- js/script.js
+++ js/script.js
@@ -5,12 +5,32 @@ document.querySelector(".search-icon").addEventListener("click", function (e) {
   searchForm.classList.toggle("active"); // Alterna a classe 'active' para mostrar ou esconder o formulário
 });
 
-const valorMinimoInput = document.getElementById("valor-minimo");
-const valorMaximoInput = document.getElementById("valor-maximo");
+const valorMinimoInput = document.getElementById('valor-minimo');
+const valorMaximoInput = document.getElementById('valor-maximo');
+const incrementMinBtn = document.getElementById('increment-min-btn');
+const decrementMinBtn = document.getElementById('decrement-min-btn');
+const incrementMaxBtn = document.getElementById('increment-max-btn');
+const decrementMaxBtn = document.getElementById('decrement-max-btn');
 
-valorMinimoInput.addEventListener("change", function () {
-  const valorMinimo = parseInt(this.value);
-  valorMaximoInput.min = valorMinimo + 50000; // O valor mínimo de "Valor Máximo" começa onde o "Valor Mínimo" termina
-  valorMaximoInput.value = valorMinimo + 50000; // Define um valor padrão para "Valor Máximo"
+incrementMinBtn.addEventListener('click', () => {
+  valorMinimoInput.value = parseInt(valorMinimoInput.value) + 50000;
 });
+
+decrementMinBtn.addEventListener('click', () => {
+  if (parseInt(valorMinimoInput.value) > parseInt(valorMinimoInput.min)) {
+    valorMinimoInput.value = parseInt(valorMinimoInput.value) - 50000;
+  }
+});
+
+incrementMaxBtn.addEventListener('click', () => {
+  valorMaximoInput.value = parseInt(valorMaximoInput.value) + 50000;
+});
+
+decrementMaxBtn.addEventListener('click', () => {
+  if (parseInt(valorMaximoInput.value) > parseInt(valorMaximoInput.min)) {
+    valorMaximoInput.value = parseInt(valorMaximoInput.value) - 50000;
+  }
+});
+
+
 
